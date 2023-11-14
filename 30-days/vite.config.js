01df28382d0c7cda18f8c0d8a4daeca0cf0ac87b
Willjfield <willjfield@gmail.com>
2023-11-14 14:33:49 -0500

    day 8

--- 30-days/vite.config.js
+++ 30-days/vite.config.js
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [glsl()],
     build: {
-        outDir: '../Day4',
+        outDir: '../Day8',
         emptyOutDir: true
     },
-    base: "https://willjfield.github.io/30-Day-Map-Challenge/Day4/",
+    base: "https://willjfield.github.io/30-Day-Map-Challenge/Day8/",
     publicPath: "./"
 });
